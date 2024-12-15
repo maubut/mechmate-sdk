4fa1174645a0726682d860da996a2ffc8fe0a20d
Mikhael Aubut <mikhaelaubut@pm.me>
2024-12-15 13:28:58 -0500

    improve sign user schema username

--- src/client/auth.ts
+++ src/client/auth.ts
@@ -1,9 +1,16 @@
 import { LoginResponse, LoginRequest } from "../schemas/login.schema";
+import { SignInUserSchema } from "../schemas/user.schema";
+import { validateRequest } from "../utils/validation";
 import { BaseClient, SDKResponse } from "./base";
 
 export class AuthClient extends BaseClient {
   async login(data: LoginRequest): Promise<SDKResponse<LoginResponse>> {
-    console.log("patate 2", data);
-    return this.fetch<SDKResponse<LoginResponse>>("/auth", "POST", data);
+    const validatedData = validateRequest(SignInUserSchema, data);
+
+    return this.fetch<SDKResponse<LoginResponse>>(
+      "/auth",
+      "POST",
+      validatedData,
+    );
   }
 }
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -16,9 +16,15 @@ const usernameRegex = /^[a-zA-Z0-9]+([_. -]?[a-zA-Z0-9])*$/;
 export const SignInUserSchema = z.object({
   username: z
     .string()
-    .min(1, "Username is required")
-    .regex(usernameRegex, "Invalid username"),
-  password: z.string(),
+    .min(1)
+    .transform((str) => str.toLowerCase())
+    .pipe(
+      z.union([
+        z.string().email("ERR_USERNAME_INVALID_FORMAT"), // Allows email format
+        z.string().regex(usernameRegex, "ERR_USERNAME_INVALID_FORMAT"), // Allows simple username format
+      ]),
+    ),
+  password: z.string().min(1),
 });
 
 export type SignInUserRequest = z.infer<typeof SignInUserSchema>;
